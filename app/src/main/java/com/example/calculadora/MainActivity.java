b94bba3be6a5952f3479025b14472538e7e7ad05
Mauricio Enrique Vasquez Ramirez <usis007620@ugb.edu.sv>
2021-02-01 20:45:13 -0600

    Calculadora basica Java

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import java.util.IllegalFormatCodePointException;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabHost tbhConversores;
+    Button btnCalcular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
-    
 }
