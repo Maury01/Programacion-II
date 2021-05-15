4a851fd4571a683d5833764a21cd3cbcf050aeb3
Mauricio Enrique Vasquez Ramirez <usis007620@ugb.edu.sv>
2021-05-14 20:17:54 -0600

    Se creo la rama del proyecto final

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,86 +1,53 @@
+
 package com.example.calculadora;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.RadioButton;
-import android.widget.Spinner;
+import android.database.Cursor;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.IllegalFormatCodePointException;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+//Mauricio Enrique Vásquez Ramirez	USIS007620
+//Michelle Brisette Perez Caballero USIS006620
+//Elias Mauricio Parada Lozano		USIS030320
+//Lisseth Alexandra Gomez Venegas	USIS005620
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-    public void Calcular(View view){
-        TextView TempVal = (TextView)findViewById(R.id.txtNum1);
-        double Num1 = Double.parseDouble(TempVal.getText().toString());
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        TempVal = (TextView)findViewById(R.id.txtNum2);
-        double Num2 = Double.parseDouble(TempVal.getText().toString());
 
-        double Respuesta = 1;
-
-        Spinner cboOperaciones = findViewById(R.id.cboOperaciones);
-        switch (cboOperaciones.getSelectedItemPosition()){
-            case 0: //Suma
-                Respuesta = Num1 +  Num2;
-                break;
-
-            case 1: //Resta
-                Respuesta = Num1 - Num2;
-                break;
-
-            case 2: //Multip.
-                Respuesta = Num1 * Num2;
-                break;
-
-            case 3: //Div.
-                Respuesta = Num1 / Num2;
-                break;
-
-            case 4: //Factorial
-                for (int i = 2; i <= Num1; i++){
-                    Respuesta *=i;
-                }
-                break;
-
-                case 5: //Porcentaje
-                    Respuesta = (Num1 / Num2) * 100;
-                    break;
-
-            case 6: //Exponente
-                Respuesta = Math.pow(Num1, Num2);
-                break;
-
-            case 7: //Raiz
-                Respuesta = Math.pow(Num1, 1/Num2);
-                break;
-
-            case 8: //Mod
-                Respuesta = Num1 % Num2;
-                break;
-
-            case 9: //Num mayor
-                if (Num1 > Num2) {
-                    Respuesta = Num1;
-                } else  {
-                    Respuesta = Num2;
-                }
-                break;
-        }
-        TempVal = findViewById(R.id.lblRespuesta);
-
-        if (cboOperaciones.getSelectedItemPosition() == 5) {
-            TempVal.setText("Respuesta: " + Respuesta + " %");
-        } else {
-            TempVal.setText("Respuesta" + Respuesta);
-        }
-    }
+	}
 }
